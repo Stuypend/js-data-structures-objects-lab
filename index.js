595eda80939c030f6231beada5c6780067f3c261
stuypend <andre@stuypend.com>
2021-04-13 17:36:05 -0400

    commit tree 562318d12b3da6916a95183c211d5669a4843b20

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+let driver = {hair: 5}
+
+function updateDriverWithKeyAndValue(driver, key, val)
+{
+  let newDriver = Object.assign({}, driver)
+  newDriver[key] = val
+
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, val)
+{
+  driver[key] = val
+
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key, val)
+{
+  let newDriver = Object.assign({}, driver)
+  delete newDriver[key]
+
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key, val)
+{
+  delete driver[key]
+
+  return driver
+}
+
